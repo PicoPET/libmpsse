be5ad42dd10f992b36a97adf7c31cb0c658beb0f
Zbigniew Chamski <zbigniew.chamski@gmail.com>
2016-03-12 22:46:38 +0100

    Control number of frames.  Store host timestamps in the host-side log.
    * src/examples/spitest.c (toplevel): Include <time.h>.
      (NUM_FRAMES): New.
      (WRITE_TIMESTAMP_TO_FILE): Likewise.
      (main): Check if user supplied an explicit number of frames.  Write the
      timestamp to output file at start of setup.  Write the start and end
      timestamp before each frame's data.
    * src/process-trace.py (toplevel): Include time and date manipulation libs.
      Read and print initial host timestamp.  Increase frame size to capture
      the host start and end timestamps.  Decode and print host timestamps of
      each frame start and end.  Report overwritten frames.  Clearly separate
      host (master) and device (slave) timestamp information.

--- src/examples/spitest.c
+++ src/examples/spitest.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <mpsse.h>
+#include <time.h>
 
 #define SIZE	0x10			// Size of SPI flash device: 1MB
 #define RCMD	"\xde\xad\xbe\xef\xca\xfe\xba\xbe"	// Standard SPI flash read command (0x03) followed by starting address (0x000000)
@@ -13,32 +14,66 @@
   #warning SWIGPYTHON enabled!
 #endif
 
+#define NUM_FRAMES     160
 #define TRANSFER_SIZE 1536
+
+/* Append current timeval to file DEST.  */
+#define WRITE_TIMESTAMP_TO_FILE(DEST) \
+do { \
+  struct timeval buf; \
+  gettimeofday (&buf, NULL); \
+  fwrite ((void *) &buf, sizeof (struct timeval), 1, DEST); \
+} while (0)
+
 unsigned char tx_buffer[TRANSFER_SIZE] = { 0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe, 0xba, 0xbe };
 
-
-int main(void)
+int main(int argc, char **argv)
 {
 	FILE *fp = NULL;
 	swig_string_data data = { 0, 0};
 	int retval = EXIT_FAILURE, dumped_bytes = 0;
+	int num_frames = NUM_FRAMES;
 	struct mpsse_context *flash = NULL;
-	
+
+	if (argc > 1)
+	{
+		int success = sscanf (argv[1], "%d", &num_frames);
+		if (!success || num_frames <= 0)
+		{
+			fprintf (stderr, "Usage: %s number_of_frames\n", argv[0]);
+			exit (1);
+		}
+	}
+
+	fp = fopen(FOUT, "wb");
+	if (!fp)
+	{
+		perror ("Cannot open FOUT");
+		exit (1);
+	}
+
+	/* Write timestamp of FTx232H init.  */
+	WRITE_TIMESTAMP_TO_FILE(fp);
+
 	if((flash = MPSSE(SPI0, 15000000, MSB)) != NULL && flash->open)
 	{
 		printf("%s initialized at %d Hz (SPI mode 0)\n", GetDescription(flash), GetClock(flash));
-		
+
 		Start(flash);
+
+		/* Write timestamp of pulling down #CS.  */
+		WRITE_TIMESTAMP_TO_FILE(fp);
+
 		data = Transfer(flash, tx_buffer, TRANSFER_SIZE);
-		//data = Read(flash, SIZE);
-		//data1 = Read(flash, SIZE);
-		//usleep (1);
+
+		/* Write timestamp of end-of-tranfer.  */
+		WRITE_TIMESTAMP_TO_FILE(fp);
+
 		Stop(flash);
 
 #if 1		
 		if(data.data)
 		{
-			fp = fopen(FOUT, "wb");
 			if(fp)
 			{
 				int i;
@@ -48,12 +83,19 @@ int main(void)
 				free(data.data);
 				dumped_bytes += data.size;
 
-				/* Exchange next 15 TRANSFER_SIZE-byte bursts, wait 100 microseconds in between.  */
-				for (i = 0; i < 15; i++)
+				/* Exchange next 'num_frames' TRANSFER_SIZE-byte bursts.  */
+				for (i = 0; i < num_frames - 1; i++)
 				{
 					Start(flash);
+
+					/* Write timestamp of pulling down #CS.  */
+					WRITE_TIMESTAMP_TO_FILE(fp);
+
 					data = Transfer(flash, tx_buffer, TRANSFER_SIZE);
-					//usleep (1);
+
+					/* Write timestamp of end-of-transfer.  */
+					WRITE_TIMESTAMP_TO_FILE(fp);
+
 					Stop(flash);
 
 					if (data.data)
--- src/process-trace.py
+++ src/process-trace.py
@@ -1,15 +1,21 @@
 from struct import *
+import time
+from datetime import date, datetime, timedelta
 
 def delta_ts_from_parts(low, high):
     return low + (high & 0xf) * 256
  
 f = open ('trace.bin', 'rb')
-frames = []
 
-frame = f.read(1536)
+(seconds, useconds) = unpack ('qq', f.read(16))
+
+print ('[HOST] Start of log: ' + str (datetime.fromtimestamp (seconds + 0.000001 * useconds)))
+
+frames = []
+frame = f.read(1536+32)
 while frame != b'':
     frames.append(frame)
-    frame = f.read(1536)
+    frame = f.read(1536+32)
 
 print ('Read %df frames. Now on to decoding...' % len(frames))
 
@@ -19,15 +25,27 @@ TS = 0
 
 def processFrame(frame):
     global duration, TS_start, TS
-    (TS, TS_at_Tx) = unpack ('II', frame[0:8])
-    print ("TS = 0x%x, TS_at_Tx = 0x%x" % (TS, TS_at_Tx))
 
-    if TS != 0xffffffff & TS != 0x0:
+    fduration = 0
+
+    (seconds, useconds) = unpack ('qq', frame[0:16])
+    print ('[HOST] Transaction: ' + str (datetime.fromtimestamp (seconds + 0.000001 * useconds)), end='')
+
+    (seconds, useconds) = unpack ('qq', frame[16:32])
+    print (' .. ' + str (datetime.fromtimestamp (seconds + 0.000001 * useconds)))
+
+    (TS, TS_at_Tx) = unpack ('II', frame[32:40])
+    print ("[SLAVE] Frame start at TS = %d (0x%x), submitted at TS = %d (0x%x)" % (TS, TS, TS_at_Tx, TS_at_Tx))
+
+    if TS != 0xffffffff and TS != 0x0:
         if TS_start == 0xffffffff:
            TS_start = TS
 
-        for i in range(8, 1536, 6):
-            if i <= 1530:
+        if TS > TS_at_Tx:
+            print ("[SLAVE] *** frame partially overwritten after being submitted for Tx")
+
+        for i in range(40, 1568, 6):
+            if i <= 1562:
                 (delta_ts_low, chan_plus_delta_ts, current, voltage) = unpack ('BBHH', frame[i:i+6])
                 if delta_ts_low == 0 and chan_plus_delta_ts == 0:
                     continue
@@ -37,7 +55,7 @@ def processFrame(frame):
                 print ((TS + delta_ts, chan, current, voltage))
                 TS += delta_ts
 
-        print ('==> Frame duration %d' % fduration)
+        print ('[SLAVE] Frame timespan: %d microseconds' % fduration)
         duration += fduration
 
 for frame in frames:
