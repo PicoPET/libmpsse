edac2c377cc73b09d8ee1a4363a54e8d8aa63588
Zbigniew Chamski <zbigniew.chamski@gmail.com>
2016-03-16 09:53:14 +0100

    [SPI Trace processing] Handle occasional 6 NUL bytes.  Refactor code.
    * src/process-trace.py (slave_frame_size): Factor out size of slave frames.
      (processFrame): If the six NUL bytes precede the actual buffer, skip them
      before decoding the slave timestamps.

--- src/process-trace.py
+++ src/process-trace.py
@@ -2,6 +2,11 @@ from struct import *
 import time
 from datetime import date, datetime, timedelta
 
+# This is the slave frame size: 12 KiB, starting with two microsecond timestamps:
+# - TS of first ADC ISR for that buffer (whether with full sample or not)
+# - TS of submission of buffer for DMA (NSS was pulled down).
+slave_frame_size = 1536 * 8
+
 vref = 2.9625  # reference "3.000" of the STM in Volt
 resistor = 0.500 #  the installed resistor
 gain = 50
@@ -20,11 +25,13 @@ f = open ('trace.bin', 'rb')
 
 print ('[HOST] Start of log: ' + str (datetime.fromtimestamp (seconds + 0.000001 * useconds)))
 
+# The 32 bytes extra are for two struct timeval in a 64-bit machine.
+# Reduce to 16 on a 32-bit machine (typically ARMs: Raspberry Pi, Odroid-C1, etc.).
 frames = []
-frame = f.read(1536*8+32)
+frame = f.read(slave_frame_size + 32)
 while frame != b'':
     frames.append(frame)
-    frame = f.read(1536*8+32)
+    frame = f.read(slave_frame_size + 32)
 
 print ('Read %df frames. Now on to decoding...' % len(frames))
 
@@ -46,8 +53,16 @@ def processFrame(frame):
     print ('[HOST] Transaction: ' + ('0x%08x:0x%08x' % (seconds_start, useconds_start)), end='')
     print (' .. ' + ('0x%08x:0x%08x' % (seconds_end, useconds_end)))
 
-    # Skip SIX 0x0 values (one sample...) spuriously received ahead of the actual STM32F4 frame.
-    (TS, TS_at_Tx) = unpack ('II', frame[38:46])
+    # Handle the (currently unexplained) case where a frame is preceded by 6 bytes at 0x0.
+    (foo, bar) = unpack ('IH', frame[32:38])
+    if foo == 0 and bar == 0:
+        frame_start = 38
+    else:
+        frame_start = 32
+
+    print ('[SLAVE] foo = 0x%x, bar = 0x%x' % (foo, bar))
+
+    (TS, TS_at_Tx) = unpack ('II', frame[frame_start:frame_start+8])
 
     # On empty frames (no SPI data on MISO) both timestamps are 0xffffffff.
     # If either differs from 0xfffffffff, it's a valid frame.
@@ -61,8 +76,8 @@ def processFrame(frame):
         if TS > TS_at_Tx:
             print ("[SLAVE] *** frame partially overwritten after being submitted for Tx")
 
-        for i in range(46, 1536*8 + 32, 6):
-            if i <= 1536*8 + 26:
+        for i in range(frame_start + 8, slave_frame_size + 32, 6):
+            if i <= slave_frame_size + 26:
                 (delta_ts, chan_plus_current, chan_plus_voltage) = unpack ('HHH', frame[i:i+6])
                 channel = (chan_plus_current & 0xf000) >> 12
                 current = rawToCurrent (chan_plus_current & 0xfff)
